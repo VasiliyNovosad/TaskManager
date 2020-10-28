cf773afc04072b38911d1bdcb9d2d9f5247155fe
Vasyl Novosad <vasyl.novosad@avenga.com>
2020-10-29 00:19:42 +0200

    added styles and markup

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+  $.ajax({
+    url: '/api/projects',
+    type: 'GET',
+    dataType: 'json',
+    success: function(json) {
+      console.log('Projects list: ', json);
+    },
+    error: function(error) {
+      console.log('Projects were not loaded', error);
+    },
+    complete: function(xhr, status) {
+      console.log('the request is complete!');
+    }
+  });
+
   $('#createProjectButton').on('click', function (e) {
     e.preventDefault();
     const projectName = $('#new-project-name').val();
@@ -10,6 +25,31 @@ $(document).ready(function() {
       dataType: 'json',
       success: function(json) {
         console.log('the project was created', json);
+        $('#createProject').modal('hide');
+        $('#new-project-name').val('');
+      },
+      error: function(error) {
+        console.log('the project was not created', error);
+      },
+      complete: function(xhr, status) {
+        console.log('the request is complete!');
+      }
+    });
+  });
+
+  $('#newProjectForm').on('submit', function (e) {
+    e.preventDefault();
+    const projectName = $('#new-project-name').val();
+    console.log({projectName, elem: $('#new-project-name')});
+    $.ajax({
+      url: '/api/projects',
+      data: {name: projectName},
+      type: 'POST',
+      dataType: 'json',
+      success: function(json) {
+        console.log('the project was created', json);
+        $('#createProject').modal('hide');
+        $('#new-project-name').val('');
       },
       error: function(error) {
         console.log('the project was not created', error);
